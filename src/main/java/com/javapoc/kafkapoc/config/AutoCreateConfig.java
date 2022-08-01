5e2e980cf366d2745a691e1912b8fee3483e0a99
Rishikesh Shetty <rishikesh.shetty@zoho.com>
2022-08-01 18:12:29 +0530

    Add existing project files to Git

---
+++ src/main/java/com/javapoc/kafkapoc/config/AutoCreateConfig.java
@@ -0,0 +1,26 @@
+package com.javapoc.kafkapoc.config;
+
+// import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.config.TopicBuilder;
+// import org.springframework.kafka.core.KafkaAdmin;
+
+// import java.util.HashMap;
+// import java.util.Map;
+
+@Configuration
+@Profile("local") //invoked only for local profile
+public class AutoCreateConfig {
+
+    @Bean
+    public NewTopic libraryEvents(){
+        return TopicBuilder.name("library-events")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+}
---
+++ src/main/java/com/javapoc/kafkapoc/domain/LibraryEventType.java
@@ -0,0 +1,7 @@
+package com.javapoc.kafkapoc.domain;
+
+
+public enum LibraryEventType {
+    NEW,
+    UPDATE
+}
